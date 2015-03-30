7c9b5f4eb18687ff301dd1605937661e2c23b216
caimmy <caimmy@hotmail.com>
2015-03-30 23:26:26 +0800

    add archgis blueprint

---
+++ archgis/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding:utf-8 _*_
+__author__ = 'caimiao'
+__date__ = '15-3-30'
+
+from flask import Blueprint, render_template
+
+archgis_app = Blueprint('archgis', __name__, template_folder='templates')
+
+@archgis_app.route('/', methods=['GET'])
+def Index():
+    return render_template('archgis/_index.html', name="hello archgis")
